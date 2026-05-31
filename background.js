6775f52824f47732469f77e7527921874d157ca1
kawa-nobu <kwnobu@gmail.com>
2026-05-31 15:21:29 +0900

    dataURLでないfaviconを取得できる様にした

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener(({ url }, sender, sendResponse) => {
+  fetch(url)
+    .then((r) => r.arrayBuffer())
+    .then((buf) => {
+      const base64 = btoa(String.fromCharCode(...new Uint8Array(buf)));
+      sendResponse(base64);
+    })
+    .catch(() => sendResponse(null));
+  return true;
+});
